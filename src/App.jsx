5e0f140daa4cb30a9b2fbda4c0d0a26a9826ce28
mijan <mijanchowdhury2001@gmail.com>
2024-09-26 01:30:15 -0400

    Initiated project and created the navbar.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import Navbar from "./navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default App;
---
+++ src/navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar" id="navbar-main">
+      <h1 className="navbar-header">#VANLIFE</h1>
+      <ul className="navbar-list">
+        <li className="navbar-li">Home</li>
+        <li className="navbar-li">About</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
